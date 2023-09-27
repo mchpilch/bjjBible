41e26bbde1991f4680957269fb8b4c245474d776
mchpilch <mchpilch@gmail.com>
2023-09-27 12:11:43 +0200

    Current route is underlined in nav-tool bar

--- src/app/pages/positionDetails/position-details.component.ts
+++ src/app/pages/positionDetails/position-details.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {PositionService} from "../../services/position/position.service";
+import {BasicPosition} from "../../models/basic-position";
 
 @Component({
   selector: 'app-position-details',
@@ -10,22 +11,37 @@ import {PositionService} from "../../services/position/position.service";
 export class PositionDetailsComponent {
   position: string = '';
 
+  private positions: BasicPosition[] | undefined;
   constructor(
     private route: ActivatedRoute,
     private positionService: PositionService
   ) {
+
   }
 
   ngOnInit(): void {
+    console.log('nOI')
     const positionParam = this.route.snapshot.queryParamMap.get('position');
 
     if (positionParam !== null) {
       this.position = positionParam;
     }
+    //
+    // console.log("Position:", this.position);
+    // console.log("Position ser:", this.positionService.getPositions().subscribe(x =>
+    // console.log(x)));
 
-    console.log("Position:", this.position);
-    console.log("Position ser:", this.positionService.getPositions().subscribe(x =>
-    console.log(x)));
+    this.positionService.getPositions().subscribe((data: BasicPosition[]) => {
+      this.positions = data;
+      console.log('ngOnInit Positions:', this.positions);
+    });
+  }
+
+  ngAfterViewInit(): void{
+   console.log('nAVI')
+
+    // console.log('nAVI' ,this.basicPosition)
+    // console.log('nAVI2' ,this.positions)
   }
 }
 
